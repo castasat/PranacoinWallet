f4e13a5431840154e088a7da80d9ba3dff9bd494
castasat <castasat@mail.ru>
2018-05-03 11:13:18 +0300

    - made singleton PranacoinWallet application class safe for threads

--- app/src/main/java/com/openyogaland/denis/pranacoinwallet/PranacoinWallet.java
+++ app/src/main/java/com/openyogaland/denis/pranacoinwallet/PranacoinWallet.java
@@ -5,29 +5,37 @@ import android.content.Context;
 import android.support.annotation.NonNull;
 
 /**
- * Singleton pattern is used
- * TODO make safe for threads
+ * Singleton pattern safe for threads
  */
 public final class PranacoinWallet extends Application
 {
   // fields
-  private static PranacoinWallet instance = null;
+  // never cached in thread memory
+  private volatile static PranacoinWallet instance = null;
   
+  // empty constructor (called from getInstance())
   public PranacoinWallet()
+  {
+  }
+  
+  // called from getPranacoinWalletContext()
+  public static PranacoinWallet getInstance()
   {
     if(instance == null)
     {
-      instance = this;
+      synchronized(PranacoinWallet.class)
+      {
+        if(instance == null)
+        {
+          instance = new PranacoinWallet();
+        }
+      }
     }
-  }
-  
-  public static PranacoinWallet getInstance()
-  {
-    return PranacoinWallet.instance;
+    return instance;
   }
   
   @NonNull
-  public static Context applicationContext()
+  static Context getPranacoinWalletContext()
   {
     return getInstance().getApplicationContext();
   }
